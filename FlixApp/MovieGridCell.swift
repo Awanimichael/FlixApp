95bb242268ada39bc85357bcec339c71993185a4
Rotimi Awani <awanimichael@gmail.com>
2019-10-24 20:29:04 -0500

    Unit2 Update

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Rotimi Awani on 10/24/19.
+//  Copyright © 2019 Rotimi Awani. All rights reserved.
+//
+
+import UIKit
+
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
